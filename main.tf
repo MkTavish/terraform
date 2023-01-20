c73ac9959267ef96a9f95effd15185d10d33dbcb
Wola Adedeji <wola.adedeji@gmail.com>
2023-01-20 11:52:31 -0800

    update lambda

--- main.tf
+++ main.tf
@@ -5,11 +5,11 @@ provider "aws" {
 
 provider "archive" {}
 
-data "archive_file" "zip" {
-  type        = "zip"
-  source_file = "hello_lambda.py"
-  output_path = "hello_lambda.zip"
-}
+# data "archive_file" "zip" {
+#   type        = "zip"
+#   source_file = "hello_lambda.py"
+#   output_path = "hello_lambda.zip"
+# }
 
 data "aws_iam_policy_document" "policy" {
   statement {
@@ -30,35 +30,40 @@ resource "aws_iam_role" "iam_for_lambda" {
   assume_role_policy = "${data.aws_iam_policy_document.policy.json}"
 }
 
-resource "aws_lambda_function" "lambda" {
-  function_name = "hello_lambda"
+# resource "aws_lambda_function" "lambda" {
+#   function_name = "hello_lambda"
 
-  filename         = "${data.archive_file.zip.output_path}"
-  source_code_hash = "${data.archive_file.zip.output_base64sha256}"
+#   filename         = "${data.archive_file.zip.output_path}"
+#   source_code_hash = "${data.archive_file.zip.output_base64sha256}"
 
-  role    = "${aws_iam_role.iam_for_lambda.arn}"
-  handler = "hello_lambda.lambda_handler"
-  runtime = "python3.9"
+#   role    = "${aws_iam_role.iam_for_lambda.arn}"
+#   handler = "hello_lambda.lambda_handler"
+#   runtime = "python3.9"
 
-  environment {
-    variables = {
-      greeting = "Hello"
-    }
-  }
-}
+#   environment {
+#     variables = {
+#       greeting = "Hello"
+#     }
+#   }
+# }
 
 data "external" "lambda_directories" {
   program = ["bash", "-c", "find ./lambda_functions -type d -mindepth 1 -maxdepth 1"]
 }
+
+data "external" "zip_lambda_code" {
+  for_each = data.external.lambda_directories.result
+  program = ["bash", "-c", "cd ${each.value} && zip -r function_code.zip ."]
+}
 resource "aws_lambda_function" "lambda_function" {
   for_each = data.external.lambda_directories.result
-  #filename = "${each.value}/function.zip"
+  filename = "${data.external.zip_lambda_code[each.key].result}"
   function_name = "${basename(each.value)}"
   role = "${aws_iam_role.iam_for_lambda.arn}"
   handler = "hello_lambda.lambda_handler"
   runtime = "python3.9"
-  source_code_hash = "${zipfile("${each.value}/")}"
-  source_code_zip = "${zipfile("${each.value}/")}"
+  source_code_hash ="${filebase64sha256("${data.external.zip_lambda_code[each.key].result}")}"
+  #source_code_zip = "${data.external.zip_lambda_code[each.key].result}"
 }
 
 
