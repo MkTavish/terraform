5234860a445d2e7616486261e96386159de23e8f
Wola Adedeji <wola.adedeji@gmail.com>
2023-01-20 12:29:41 -0800

    update lambda

--- main.tf
+++ main.tf
@@ -47,40 +47,45 @@ resource "aws_iam_role" "iam_for_lambda" {
 #   }
 # }
 
-data "external" "lambda_directories" {
-  program = ["bash", "-c", "find ./lambda_functions -type d -mindepth 1 -maxdepth 1"]
-}
+# data "external" "lambda_directories" {
+#   program = ["bash", "-c", "find ./lambda_functions -type d -mindepth 1 -maxdepth 1"]
+# }
 
-data "external" "zip_lambda_code" {
-  for_each = data.external.lambda_directories.result
-  program = ["bash", "-c", "cd ${each.value} && zip -r function_code.zip ."]
-}
-resource "aws_lambda_function" "lambda_function" {
-  for_each = data.external.lambda_directories.result
-  filename = "${data.external.zip_lambda_code[each.key].result}"
-  function_name = "${basename(each.value)}"
-  role = "${aws_iam_role.iam_for_lambda.arn}"
-  handler = "hello_lambda.lambda_handler"
-  runtime = "python3.9"
-  source_code_hash ="${filebase64sha256("${data.external.zip_lambda_code[each.key].result}")}"
-  #source_code_zip = "${data.external.zip_lambda_code[each.key].result}"
-}
+# data "external" "zip_lambda_code" {
+#   for_each = data.external.lambda_directories.result
+#   program = ["bash", "-c", "cd ${each.value} && zip -r function_code.zip ."]
+# }
 
 
-
-# resource "aws_lambda_function" "lambda2" {
-#   function_name = "hello_lambda2"
-
-#   filename         = "${data.archive_file.zip.output_path}"
-#   source_code_hash = "${data.archive_file.zip.output_base64sha256}"
-
-#   role    = "${aws_iam_role.iam_for_lambda.arn}"
+# resource "aws_lambda_function" "lambda_function" {
+#   for_each = data.external.lambda_directories.result
+#   filename = "${data.external.zip_lambda_code[each.key].result}"
+#   function_name = "${basename(each.value)}"
+#   role = "${aws_iam_role.iam_for_lambda.arn}"
 #   handler = "hello_lambda.lambda_handler"
 #   runtime = "python3.9"
-
-#   environment {
-#     variables = {
-#       greeting = "Hello"
-#     }
-#   }
+#   source_code_hash ="${filebase64sha256("${data.external.zip_lambda_code[each.key].result}")}"
+#   #source_code_zip = "${data.external.zip_lambda_code[each.key].result}"
 # }
+
+data "external" "lambda_directories" {
+  program = ["bash", "-c", "find ./lambda_functions -type d -mindepth 1 -maxdepth 1 -print0"]
+}
+
+resource "aws_lambda_function" "lambda_function" {
+  for_each = data.external.lambda_directories.result
+
+  function_name = "${basename(each.value)}"
+  role = ""
+  handler = "hello_lambda.lambda_handler"
+  runtime = "python3.9"
+  source_code_hash = "${data.archive_file.lambda_function_code[each.key].output_sha256}"
+  filename = "${data.archive_file.lambda_function_code[each.key].output_path}"
+}
+
+data "archive_file" "lambda_function_code" {
+  for_each = data.external.lambda_directories.result
+  type        = "zip"
+  source_dir  = "${each.value}"
+  output_path = "${each.value}/function_code.zip"
+}
