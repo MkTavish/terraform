01a03cc85c38759de2fc174c9ca904ff2db514a3
Wola Adedeji <wola.adedeji@gmail.com>
2023-01-20 11:33:20 -0800

    add lambda monorepo

--- main.tf
+++ main.tf
@@ -47,6 +47,22 @@ resource "aws_lambda_function" "lambda" {
   }
 }
 
+data "external" "lambda_directories" {
+  program = ["bash", "-c", "find ./lambda_functions -type d -mindepth 1 -maxdepth 1"]
+}
+resource "aws_lambda_function" "lambda_function" {
+  for_each = data.external.lambda_directories.result
+  #filename = "${each.value}/function.zip"
+  function_name = "${basename(each.value)}"
+  role = "${aws_iam_role.iam_for_lambda.arn}"
+  handler = "hello_lambda.lambda_handler"
+  runtime = "python3.9"
+  source_code_hash = "${zipfile("${each.value}/")}"
+  source_code_zip = "${zipfile("${each.value}/")}"
+}
+
+
+
 # resource "aws_lambda_function" "lambda2" {
 #   function_name = "hello_lambda2"
 
