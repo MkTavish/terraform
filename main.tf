82916e89c29599e33f77aac89482f6a69f639086
Wola Adedeji <wola.adedeji@gmail.com>
2023-01-22 13:29:39 -0800

    update lambda

--- main.tf
+++ main.tf
@@ -68,24 +68,46 @@ resource "aws_iam_role" "iam_for_lambda" {
 #   #source_code_zip = "${data.external.zip_lambda_code[each.key].result}"
 # }
 
-data "external" "lambda_directories" {
-  program = ["bash", "-c", "find ./lambda_functions -type d -mindepth 1 -maxdepth 1 -print0"]
+# data "external" "lambda_directories" {
+#   program = ["bash", "-c", "find ./lambda_functions -type d -mindepth 1 -maxdepth 1 -print0"]
+# }
+
+# resource "aws_lambda_function" "lambda_function" {
+#   for_each = data.external.lambda_directories.result
+
+#   function_name = "${basename(each.value)}"
+#   role = ""
+#   handler = "hello_lambda.lambda_handler"
+#   runtime = "python3.9"
+#   source_code_hash = "${data.archive_file.lambda_function_code[each.key].output_sha256}"
+#   filename = "${data.archive_file.lambda_function_code[each.key].output_path}"
+# }
+
+# data "archive_file" "lambda_function_code" {
+#   for_each = data.external.lambda_directories.result
+#   type        = "zip"
+#   source_dir  = "${each.value}"
+#   output_path = "${each.value}/function_code.zip"
+# }
+
+resource "null_resource" "zip_lambda" {
+  # triggers = {
+  #   lambda_function_sha1 = sha1(file("${path.module}/lambda_functions/**"))
+  # }
+  provisioner "local-exec" {
+    command = <<CMD
+    for i in lambda_functions/*/; do zip -r "$${i%/}.zip" "$i"; done
+    CMD
+  }
 }
 
 resource "aws_lambda_function" "lambda_function" {
-  for_each = data.external.lambda_directories.result
-
-  function_name = "${basename(each.value)}"
-  role = ""
+  depends_on  = [null_resource.zip_lambda]
+  for_each = fileset(path.module, "lambda_functions/*.zip")
+  filename = "${each.key}"
+  function_name = "${replace(replace(each.key, "lambda_functions/", ""), ".zip", "")}"
+  role = "${aws_iam_role.iam_for_lambda.arn}"
   handler = "hello_lambda.lambda_handler"
   runtime = "python3.9"
-  source_code_hash = "${data.archive_file.lambda_function_code[each.key].output_sha256}"
-  filename = "${data.archive_file.lambda_function_code[each.key].output_path}"
-}
-
-data "archive_file" "lambda_function_code" {
-  for_each = data.external.lambda_directories.result
-  type        = "zip"
-  source_dir  = "${each.value}"
-  output_path = "${each.value}/function_code.zip"
+  source_code_hash = filebase64sha256("${each.key}")
 }
